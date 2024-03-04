509194bf1a75009dd15eb3461feaaf6368c4387a
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-03-04 14:47:47 +0300

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	for (const value of array) {
-		array[array.indexOf(value)] = appendString + value;
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
